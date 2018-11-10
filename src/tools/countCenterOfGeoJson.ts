daeefa04a71c5091f999cc2330539be9e5d375e9
Pavol Hejny <me@pavolhejny.com>
2018-11-10 15:22:50 +0100

    real center

--- src/tools/countCenterOfGeoJson.ts
+++ src/tools/countCenterOfGeoJson.ts
@@ -2,17 +2,16 @@ import { ICoordinates } from '../view/maps/ICoordinates';
 import { OSMGeoJson, OSMCoordinate } from './fetchCity';
 
 export function countCenterOfGeoJson(geoJson: OSMGeoJson): ICoordinates {
-
-    let coordsAggregated:[number,number] = [0,0];
-    for(const coords of geoJson.coordinates[0]){
-        coordsAggregated[0]+=coords[0];
-        coordsAggregated[1]+=coords[1];
+    let coordsAggregated: [number, number] = [0, 0];
+    for (const coords of geoJson.coordinates[0]) {
+        coordsAggregated[0] += coords[0];
+        coordsAggregated[1] += coords[1];
     }
 
-    coordsAggregated[0]/=geoJson.coordinates[0].length;
-    coordsAggregated[1]/=geoJson.coordinates[0].length;
+    coordsAggregated[0] /= geoJson.coordinates[0].length;
+    coordsAggregated[1] /= geoJson.coordinates[0].length;
 
-    return getCoordinates(coordsAggregated);//todo real center
+    return getCoordinates(coordsAggregated);
 }
 
 export function getCoordinates(osmCoordinates: OSMCoordinate): ICoordinates {
